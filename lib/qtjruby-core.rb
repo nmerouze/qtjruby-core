b632e7298cdc947eb46865cd6959056cc541d803
Nicolas Mérouze <nicolas.merouze@gmail.com>
2008-08-21 11:48:16 +0200

    Fix bug due to a change in JRuby 1.1.3.

--- lib/qtjruby-core.rb
+++ lib/qtjruby-core.rb
@@ -1,4 +1,7 @@
 require 'java'
+
+Dir.glob(ENV_JAVA['jruby.home'] + '/lib/qtjambi-*.jar') { |jar| require jar }
 require 'qtjruby-core.jar'
+
 require 'qtjruby-core/core_ext'
 require 'qtjruby-core/qt'
